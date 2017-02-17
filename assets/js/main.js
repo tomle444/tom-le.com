66f4d913840f8a3d576d3132a99c6f1a5af010e4
Tom Le <tomle444@gmail.com>
2017-02-17 02:06:44 -0800

    Resolve Errors missing images, Footer updates

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,5 +1,5 @@
 $(window).on('load', function() { // makes sure the whole site is loaded 
-	$('#status').fadeOut(); // will first fade out the loading animation 
-	$('#preloader').delay(350).fadeOut('slow'); // will fade out the white DIV that covers the website. 
-	$('body').delay(350).css({'overflow':'visible'});
+	$('#status').delay(550).fadeOut(); // will first fade out the loading animation 
+	$('#preloader').delay(950).fadeOut('slow'); // will fade out the white DIV that covers the website. 
+	$('body').delay(950).css({'overflow':'visible'});
 })
